728a301aa249d3520be5b1d7ca697c395bf4db9c
bilrazor <113417155+bilrazor@users.noreply.github.com>
2023-01-25 14:49:28 +0100

    users

--- gimnasio/RestAPI/webserviceapp/views.py
+++ gimnasio/RestAPI/webserviceapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+
 #models
 from .models import Tpersona
 # Create your views here.
@@ -21,3 +27,27 @@ def profile(request):
 		respuesta_final.append(diccionario)
 		
 	return JsonResponse(respuesta_final, safe=False)
+
+
+
+
+    def users(request):
+        correo = request.POST.get('correo')
+        password = request.POST.get('password')
+        if not correo or not password:
+            return JsonResponse({'error': 'Email and password are required'}, status=400)
+	
+        
+        # check if the email is already registered
+        if User.objects.filter(correo=correo).exists():
+            return JsonResponse({'error': 'Email already registered'}, status=400)
+        
+        # create a new user
+        user = Tpersona.objects.create_user(nombre=nombre, correo=correo, password=password)
+        user.save()
+        
+        # authenticate the user and log them in
+        new_user = authenticate(correo=correo, password=password)
+        login(request, new_user)
+        
+        return JsonResponse({'message': 'Registration successful'}, status=201)
