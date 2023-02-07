9b23e3ec9fb4162d4feb3001f77d15df7a7dcc90
bilrazor <bhanovert@fpcoruna.afundacion.org>
2023-02-07 14:49:47 +0100

    comentarios puestos

--- gimnasio2/RestAPI/webserviceapp/views.py
+++ gimnasio2/RestAPI/webserviceapp/views.py
@@ -26,7 +26,14 @@ def users(request):
         if password != passwordConfirm:
             return JsonResponse({'error': 'Las contraseñas no coinciden'}, status=400)
         
-    
+        # Validar que la contraseña tenga al menos 8 caracteres
+        if len(password) < 8:
+            return JsonResponse({'error': 'La contraseña debe tener al menos 8 caracteres'}, status=400)
+        # Validar que el correo sea de Gmail o Hotmail
+        
+        if not correo.endswith('@gmail.com') and not correo.endswith('@hotmail.com'):
+            return JsonResponse({'error': 'El correo debe ser de Gmail o Hotmail'}, status=400)
+        
         # Validar que el usuario no exista
         if Tpersona.objects.filter(correo=correo).exists():
             return JsonResponse({'error': 'El usuario ya existe'}, status=409)
@@ -36,63 +43,50 @@ def users(request):
         user.set_password(password)
         user.save()
 
-        # Crear y devolver el token de sesion
-        # session_token = create_session_token(user)
-        # user.session_token = session_token
-        # user.save()
-        # return JsonResponse({'sessionToken': session_token}, status=201)
+      
         return JsonResponse({'OK': 'El usuario registrado'}, status=201)
 
     return HttpResponse(status=405)
 
-
-"""@csrf_exempt
-def sessions(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        correo = data.get('correo')
-        password = data.get('password')
-    
-        if not all([correo, password]):
-            return JsonResponse({'error': 'Parámetros faltantes: correo y password son requeridos'}, status=400)
-        
-        try:
-            user = Tpersona.objects.get(correo=correo)
-        except Tpersona.DoesNotExist:
-            return JsonResponse({'error': 'Usuario no encontrado'}, status=401)
-        
-        if check_password(password, user.password):
-            session_token = uuid.uuid4()
-            Tpersona.objects.filter(idpersona=user.idpersona).update(session_token=str(session_token))
-            
-            data = {'idpersona': user.idpersona, 'sessionToken': str(session_token)}
-          
-        else:
-            return JsonResponse({'error': 'Contraseña incorrecta'}, status=401)
-            
-    return JsonResponse(data, status=200)"""
+ 
     
 @csrf_exempt
 def sessions(request):
+    #Verificar si el método de la petición es POST. Si no lo es, no hace nadaç
+    
     if request.method == 'POST':
+        
+        #Carga los datos enviados en el cuerpo de la petición como un diccionario de 
+        # Python utilizando json.loads(request.body).
         data = json.loads(request.body)
+        
+        #Obtiene los valores de "correo" y "password" del diccionario.
         correo = data.get('correo')
         password = data.get('password')
-    
+        #Verifica si ambos valores existen. Si alguno de ellos falta, devuelve una 
+        # respuesta JSON con un error indicando que faltan parámetros.
         if not all([correo, password]):
             return JsonResponse({'error': 'Parámetros faltantes: correo y password son requeridos'}, status=400)
-        
+        #Intenta obtener un usuario de la base de datos con el correo especificado. 
+        # Si el usuario no existe, devuelve  una respuesta JSON con un error indicando 
+        # que el usuario no fue encontrado.
         try:
             user = Tpersona.objects.get(correo=correo)
         except Tpersona.DoesNotExist:
             return JsonResponse({'error': 'Usuario no encontrado'}, status=401)
-        
+        #Verifica si la contraseña enviada coincide con la contraseña almacenada 
+        # en la base de datos para ese usuario. Si no coincide, devuelve una respuesta JSON con un error indicando que la contraseña es incorrecta.
         if check_password(password, user.password):
+            #Genera un token de sesión único y una fecha de expiración para ese token.
             session_token = uuid.uuid4()
             expires_at = datetime.datetime.now() + datetime.timedelta(minutes=1)            
             Tpersona.objects.filter(idpersona=user.idpersona).update(session_token=str(session_token), expires_at=expires_at)
+            #Verifica si la fecha actual es mayor que la fecha de expiración. Si es así, devuelve una 
+            # respuesta JSON con un error indicando que el token ha caducado.
             if datetime.datetime.now()  > expires_at:
                 return JsonResponse({'error': 'Token ha caducado, inicia sesión de nuevo'}, status=401)        
+            #Devuelve una respuesta JSON con los datos del usuario, incluyendo el ID de la 
+            # persona, el token de sesión y la fecha de expiración.
             data = {'idpersona': user.idpersona, 'sessionToken': str(session_token), 'expiresAt': expires_at.isoformat()}
           
         else:
@@ -101,93 +95,23 @@ def sessions(request):
     
     return JsonResponse(data, status=200)
     
-    
- #@csrf_exempt
-#def sessions(request):
- #   if request.method == 'POST':
-  #      data = json.loads(request.body)
-  #      correo = data.get('correo')
-  #      password = data.get('password')
-#
- #       user = authenticate(request, correo=correo, password=password)
- #       if user is None:
- #           return JsonResponse({'error': 'Credenciales inválidas'}, status=401)
- #       login(request, user)
 
-        #generate session token
-#        session_token = uuid.uuid4()
-#
- #       return JsonResponse({'session_token': str(session_token)}, status=200)
-  #  else:
-  #      return JsonResponse({'error': 'Método no permitido'}, status=405)
-  
- 
-"""  
-@login_required
-def profile(request):
-    # Recuperar el token de sesión de la solicitud
-    session_token = request.headers.get('sessionToken')
-    # Verificar si el token de sesión es válido
-    if session_token is None:
-        return JsonResponse({"error": "Token de sesión inválido"}, status=401)
-    # Recuperar el perfil del usuario autenticado
-    user = request.user
-    profile = {
-        "Dni": user.dni,
-        "nombre": user.nombre,
-        "correo": user.correo,
-        "direccion": user.direccion,
-        "telefono": user.telefono
-    }
-    
-    return JsonResponse(profile)
- 
-@login_required
-def profile2(request):
-    profile = Tpersona.objects.get(user=request.user)
-    context = {
-        'dni': profile.dni,
-        'nombre': profile.nombre,
-        'correo': profile.correo,
-        'direccion': profile.direccion,
-        'telefono': profile.telefono,
-    }
-    return render(request, 'user_profile.html', context)
-""" 
-"""
+
 @csrf_exempt
+
 def profile(request,idpersona):
-    if request.method == 'GET':
-        session_token = request.headers.get("sessionToken")
-         if not session_token or session_token != token_en_la_bd:
-            return JsonResponse({"error": "Token de sesión inválido"}, status=401)
         
-        user = Tpersona.objects.get(idpersona=idpersona)
-        if not session_token or session_token != user.session_token:
-            return JsonResponse({"error": "Token de sesión inválido"}, status=401)
-       
-      
-        data = {
-        'dni': user.dni,
-        'nombre':user.nombre,
-        'correo': user.correo,
-        'direccion': user.direccion,
-        'telefono': user.telefono
-    }
-    
-    return JsonResponse(data)"""
-
-@csrf_exempt
-def profile(request,idpersona):
-    
+    # La función verifica si la solicitud es de tipo GET.
     if request.method == 'GET':
+        #Luego, obtiene el valor del encabezado "sessionToken" de la solicitud.
         session_token = request.headers.get('sessionToken')
         print(session_token)
+        #Busca un objeto de la tabla Tpersona con el idpersona proporcionado en la URL.
         user = Tpersona.objects.get(idpersona=idpersona)
-        
+        #Verifica si el valor del encabezado "sessionToken" es válido comparándolo con el session_token almacenado en la tabla Tpersona.
         if not session_token or session_token != user.session_token:
             return JsonResponse({"error": "Token de sesión inválido"}, status=401)
-        
+        #Si el token de sesión es válido, se crea un diccionario con los datos del usuario (dni, nombre, correo, dirección, teléfono y contraseña).
         data = {
             'dni': user.dni,
             'nombre':user.nombre,
@@ -196,35 +120,18 @@ def profile(request,idpersona):
             'telefono': user.telefono,
             'password':user.password
         }
-    
+        #Finalmente, se devuelve una respuesta JSON con los datos del usuario.
         return JsonResponse(data)
 
-
-""" def profile(request):
-    if request.method == 'GET':
-        session_token = request.headers.get("sessionToken")
-        idpersona = request.headers.get("IdPersona")
-        
-        try:
-            # Validar sesión
-            user = Tpersona.objects.get(idpersona=idpersona, session_token=session_token)
-        except Tpersona.DoesNotExist:
-            return JsonResponse({'error': 'Sesión inválida'}, status=401)
-        
-        try:
-            persona = Tpersona.objects.get(idpersona=idpersona)
-            data = {'nombre': persona.nombre, 'correo': persona.correo}
-            return JsonResponse(data, status=200)
-        except Tpersona.DoesNotExist:
-            return JsonResponse({'error': 'La persona no existe'}, status=404)"""
+ 
             
 @csrf_exempt         
 def datos(request, idpersona):
 
     if request.method == 'PUT':
-# Recupera los datos enviados en la solicitud
+        # Recupera los datos enviados en la solicitud
         data = json.loads(request.body.decode("utf-8"))
-# Recupera el usuario con idpersona
+        # Recupera el usuario con idpersona
         try:
             user = Tpersona.objects.get(idpersona=idpersona)
         except Tpersona.DoesNotExist:
@@ -251,32 +158,4 @@ def datos(request, idpersona):
         user.save()
         # Devuelve una respuesta vacía para indicar que la operación se realizó con éxito
         return JsonResponse({'mensaje': 'Perfil actualizado exitosamente'}, status=200)
-    
-"""   
-@csrf_exempt
-def datos(request, idpersona):
-    if request.method == 'PUT':
-        data = json.loads(request.body.decode("utf-8"))
-        user = get_user(idpersona)
-        if not user:
-            return JsonResponse({'error': f'El usuario con idpersona {idpersona} no existe'}, status=400)
-        update_user(user, data)
-        return JsonResponse({'mensaje': 'Perfil actualizado exitosamente'}, status=200)
-
-def get_user(idpersona):
-    try:
-        return Tpersona.objects.get(idpersona=idpersona)
-    except Tpersona.DoesNotExist:
-        return None
-
-def update_user(user, data):
-    user.nombre = data.get("nombre", user.nombre)
-    user.correo = data.get("correo", user.correo)
-    user.dni = data.get("dni", user.dni)
-    user.direccion = data.get("direccion", user.direccion)
-    user.telefono = data.get("telefono", user.telefono)
-    password = data.get("password")
-    if password:
-        user.password = password
-        user.set_password(password)
-    user.save()"""
+ 
